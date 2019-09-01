7754a8c3a18fbe609536e6be2afdc0f02e9fcc6f
rednafi <redowan.nafi@gmail.com>
2019-09-01 17:43:57 +0600

    added docker deployment script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CONTAINER_NAME=flask-tmplt
+TAG=flask
+DAEMON=$1
+
+sudo docker stop $CONTAINER_NAME
+sudo docker rm $CONTAINER_NAME
+sudo docker build . --tag=$TAG
+sudo docker run $DAEMON -p 5000:5000 --name $CONTAINER_NAME $TAG 
--- flask-ascii-art/run_flask.py
+++ flask-ascii-art/run_flask.py
@@ -4,6 +4,8 @@ from app import create_app
 # environment = os.environ.get("ENVIRONMENT", "PRODUCTION")
 environment = "DEVELOPMENT"
 application = create_app()
+
+# If being deployed via docker, comment out this portion
 if environment != "PRODUCTION":
     if __name__ == "__main__":
         application.run(host="0.0.0.0", port=5000, debug=True)
