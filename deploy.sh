e43ad53f08b3103d1efbf98a724204873ffd3132
rednafi <redowan.nafi@gmail.com>
2019-09-01 18:00:17 +0600

    added docker image removal

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ DAEMON=$1
 
 sudo docker stop $CONTAINER_NAME
 sudo docker rm $CONTAINER_NAME
+sudo docker rmi $TAG 
 sudo docker build . --tag=$TAG
 sudo docker run $DAEMON -p 5000:5000 --name $CONTAINER_NAME $TAG 
